bd6c730b67279d200869e48b7a8440908415e785
Jim Cramer <remarcmij@gmail.com>
2024-09-24 10:37:28 +0200

    Add setup script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
